910111f818066378dfaf13989265f538ef880e9e
Aashish Dhawan <aashishdhawan@gmail.com>
2022-12-24 13:53:25 +0530

    added products pages

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
+  { href: '/products', title: 'Products' },
   { href: '/about', title: 'About' },
 ]
 
--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,27 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Skia - A remote team management solution',
+    description: `Skia helps you to track project progress, plan resource allocation and keeps you updated with various reports.`,
+    imgSrc: '/static/images/products/skia.jpg',
+    href: 'https://www.skia.me',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Bugplot - A test case management solution',
+    description: `Bugplot helps you to documents all manual test cases of a project. You can initiate test runs and generate regression reports.`,
+    imgSrc: '/static/images/products/bugplot.jpg',
+    href: 'https://www.bugplot.com',
+  },
+  {
+    title: 'Kutumb - A personal relationship management solution',
+    description: `Kutumb helps you to keep track of your network, reminds you about the important dates and when to follow up with your connections.`,
+    imgSrc: '/static/images/products/kutumb.jpg',
+    href: 'https://www.kutumb.me',
+  },
+  {
+    title: 'Quicklook - One page personal profile builder',
+    description: `Quicklook helps you to build your one page personal profile where you can link all your social profiles along with adding other important details.`,
+    imgSrc: '/static/images/products/quicklook.jpg',
+    href: 'https://www.quicklook.me',
   },
 ]
 
