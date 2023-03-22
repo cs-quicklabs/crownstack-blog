9f7ab656e22fd88c98f950a6ceee68ccac2b76c8
AnkurSharmaCrownstack <ankur.sharma@crownstack.com>
2023-03-22 11:41:47 +0530

    fix: 404 page button color

--- pages/404.js
+++ pages/404.js
@@ -20,7 +20,7 @@ export default function FourZeroFour() {
             But dont worry, you can find plenty of other things on our homepage.
           </p>
           <Link href="/">
-            <button className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
+            <button className="focus:shadow-outline-primary inline rounded-lg border border-transparent bg-primary px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-primary hover:bg-opacity-90 focus:outline-none dark:hover:bg-primary">
               Back to homepage
             </button>
           </Link>
