6ba38bcd59c400185a1727244910dc977ddc21a3
Aashish Dhawan <aashishdhawan@gmail.com>
2023-03-31 19:52:01 +0530

    Move blog link to home link to reduce duplication

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,5 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/products', title: 'Products' },
   { href: '/about', title: 'About' },
