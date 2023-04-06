54df54201fbe1205d87950396a3a7d50b77812df
Aashish Dhawan <aashishdhawan@gmail.com>
2023-04-06 18:06:13 +0530

    Add careers link in header

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -67,7 +67,7 @@ const LayoutWrapper = ({ children }) => {
             <ThemeSwitch />
             <div>
               <div className="prose ml-2 text-lg text-primary hover:text-primary hover:underline dark:hover:text-primary">
-                <a href="https://calendly.com/aashish-dhawan/15min">Talk to Us → </a>
+                <a href="https://calendly.com/aashish-dhawan/15min">Talk to Sales → </a>
               </div>
             </div>
             <MobileNav />
--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/products', title: 'Products' },
   { href: '/about', title: 'About' },
+  { href: 'https://crownstack.zohorecruit.com/jobs/Careers', title: 'Careers' },
 ]
 
 export default headerNavLinks
