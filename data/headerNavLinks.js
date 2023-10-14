a26a6cd8fefc56d7cbc3c0aa17cbe51b584bc19d
Aashish Dhawan <aashishdhawan@gmail.com>
2023-10-14 18:44:02 +0530

    remove About link from nav Bar

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/tags', title: 'Tags' },
   { href: 'https://www.quicklabs.in', title: 'Products' },
-  { href: '/about', title: 'About' },
   { href: '/newsletters', title: 'Newsletters' },
 ]
 
