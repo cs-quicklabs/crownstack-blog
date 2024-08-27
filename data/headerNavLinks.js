35ab222d343e6b35b7cdf4ba01156be1ba001578
Aashish Dhawan <aashishdhawan@gmail.com>
2024-08-27 13:42:50 +0530

    remove newsletters

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/tags', title: 'Tags' },
   { href: 'https://www.quicklabs.in', title: 'Products' },
-  { href: '/newsletters', title: 'Newsletters' },
 ]
 
 export default headerNavLinks
