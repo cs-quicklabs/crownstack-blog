ffcd2646f5865e5e685ef10bc9a090f0b5abb573
Aashish Dhawan <aashishdhawan@gmail.com>
2024-08-27 13:48:48 +0530

    Update URL

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://blog.crownstack.com',
-  siteRepo: 'https://github.com/aashishdhawan/crownstack-tailwind-blog',
+  siteRepo: 'https://github.com/aashishdhawan/crownstack-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/social-banner.png',
